24ab5242fb668c3ae801f0b13f63b4090bf91ced
oscarjes <oscar@jesionek.co>
2018-02-17 15:20:50 +0700

    initial commit

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/Oscar/programming/personal-blog/src/layouts/index.jsx"
+  import data from "/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
---
+++ .cache/sync-requires.js
@@ -0,0 +1,82 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.layouts = {
+  "layout---index": preferDefault(require("/Users/Oscar/programming/personal-blog/.cache/layouts/index.js"))
+}
+
+exports.components = {
+  "component---src-templates-post-template-jsx": preferDefault(require("/Users/Oscar/programming/personal-blog/src/templates/post-template.jsx")),
+  "component---src-templates-tag-template-jsx": preferDefault(require("/Users/Oscar/programming/personal-blog/src/templates/tag-template.jsx")),
+  "component---src-templates-category-template-jsx": preferDefault(require("/Users/Oscar/programming/personal-blog/src/templates/category-template.jsx")),
+  "component---src-templates-page-template-jsx": preferDefault(require("/Users/Oscar/programming/personal-blog/src/templates/page-template.jsx")),
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/Oscar/programming/personal-blog/.cache/dev-404-page.js")),
+  "component---src-pages-404-jsx": preferDefault(require("/Users/Oscar/programming/personal-blog/src/pages/404.jsx")),
+  "component---src-pages-categories-jsx": preferDefault(require("/Users/Oscar/programming/personal-blog/src/pages/categories.jsx")),
+  "component---src-pages-index-jsx": preferDefault(require("/Users/Oscar/programming/personal-blog/src/pages/index.jsx")),
+  "component---src-pages-tags-jsx": preferDefault(require("/Users/Oscar/programming/personal-blog/src/pages/tags.jsx"))
+}
+
+exports.json = {
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "posts-perfecting-the-art-of-perfection.json": require("/Users/Oscar/programming/personal-blog/.cache/json/posts-perfecting-the-art-of-perfection.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "tags-handwriting.json": require("/Users/Oscar/programming/personal-blog/.cache/json/tags-handwriting.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "tags-learning-to-write.json": require("/Users/Oscar/programming/personal-blog/.cache/json/tags-learning-to-write.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "categories-design-inspiration.json": require("/Users/Oscar/programming/personal-blog/.cache/json/categories-design-inspiration.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "posts-the-origins-of-social-stationery-lettering.json": require("/Users/Oscar/programming/personal-blog/.cache/json/posts-the-origins-of-social-stationery-lettering.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "tags-typefaces.json": require("/Users/Oscar/programming/personal-blog/.cache/json/tags-typefaces.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "tags-lettering.json": require("/Users/Oscar/programming/personal-blog/.cache/json/tags-lettering.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "tags-history.json": require("/Users/Oscar/programming/personal-blog/.cache/json/tags-history.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "categories-design-culture.json": require("/Users/Oscar/programming/personal-blog/.cache/json/categories-design-culture.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "contact.json": require("/Users/Oscar/programming/personal-blog/.cache/json/contact.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "posts-humane-typography-in-the-digital-age.json": require("/Users/Oscar/programming/personal-blog/.cache/json/posts-humane-typography-in-the-digital-age.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "tags-design.json": require("/Users/Oscar/programming/personal-blog/.cache/json/tags-design.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "tags-typography.json": require("/Users/Oscar/programming/personal-blog/.cache/json/tags-typography.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "tags-web-development.json": require("/Users/Oscar/programming/personal-blog/.cache/json/tags-web-development.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "categories-typography.json": require("/Users/Oscar/programming/personal-blog/.cache/json/categories-typography.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "about.json": require("/Users/Oscar/programming/personal-blog/.cache/json/about.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "posts-a-brief-history-of-typography.json": require("/Users/Oscar/programming/personal-blog/.cache/json/posts-a-brief-history-of-typography.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "tags-linotype.json": require("/Users/Oscar/programming/personal-blog/.cache/json/tags-linotype.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "tags-monotype.json": require("/Users/Oscar/programming/personal-blog/.cache/json/tags-monotype.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "tags-history-of-typography.json": require("/Users/Oscar/programming/personal-blog/.cache/json/tags-history-of-typography.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "tags-helvetica.json": require("/Users/Oscar/programming/personal-blog/.cache/json/tags-helvetica.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "posts-the-birth-of-movable-type.json": require("/Users/Oscar/programming/personal-blog/.cache/json/posts-the-birth-of-movable-type.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "tags-open-source.json": require("/Users/Oscar/programming/personal-blog/.cache/json/tags-open-source.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "tags-gatsby.json": require("/Users/Oscar/programming/personal-blog/.cache/json/tags-gatsby.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("/Users/Oscar/programming/personal-blog/.cache/json/dev-404-page.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "404.json": require("/Users/Oscar/programming/personal-blog/.cache/json/404.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "categories.json": require("/Users/Oscar/programming/personal-blog/.cache/json/categories.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/index.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "tags.json": require("/Users/Oscar/programming/personal-blog/.cache/json/tags.json"),
+  "layout-index.json": require("/Users/Oscar/programming/personal-blog/.cache/json/layout-index.json"),
+  "404-html.json": require("/Users/Oscar/programming/personal-blog/.cache/json/404-html.json")
+}
