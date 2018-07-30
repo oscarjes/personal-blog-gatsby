8783c22d49f40394257d1df14b65cfcae6fc9f2a
Oscar Jesionek <oscar@jesionek.co>
2018-07-30 09:06:08 +0700

    .gitignore is now working

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/oj/code/personal/personal-blog-gatsby/src/layouts/index.jsx"
-  import data from "/Users/oj/code/personal/personal-blog-gatsby/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
